e1b0d356baad922cd652f3aa8a54fe6b4fe664c8
Nicolas ades <nicolas.ades@epitech.eu>
2014-04-28 15:24:36 +0200

    nico

--- includes/env.h
+++ includes/env.h
@@ -5,11 +5,17 @@
 ** Login   <nicolas@epitech.net>
 ** 
 ** Started on  Mon Apr 28 13:49:50 2014 Nicolas Ades
-** Last update Mon Apr 28 14:04:04 2014 Nicolas Ades
+** Last update Mon Apr 28 15:22:50 2014 Nicolas Ades
 */
 
 #ifndef ENV_H_
 # define ENV_H_
 # include "main.h"
+
+typedef struct	s_env
+{
+  char		**env;
+  char		**path;
+}		t_env;
 
 #endif /* ENV_H_ */
---
+++ includes/parser.h
@@ -0,0 +1,20 @@
+/*
+** parser.h for  in /home/nicolas/Workspace/42sh/includes
+** 
+** Made by Nicolas Ades
+** Login   <nicolas@epitech.net>
+** 
+** Started on  Mon Apr 28 15:22:59 2014 Nicolas Ades
+** Last update Mon Apr 28 15:24:11 2014 Nicolas Ades
+*/
+
+#ifndef PARSER_H_
+# define PARSER_H_
+# include "main.h"
+
+typedef struct	s_info
+{
+  
+}		t_info;
+
+#endif /* PARSER_H_ */
