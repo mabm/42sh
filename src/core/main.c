b78b8cfd95c0bc3018238e3d603f65ffd6dd7d75
Nicolas ades <nicolas.ades@epitech.eu>
2014-04-29 12:01:51 +0200

    nico

--- src/core/main.c
+++ src/core/main.c
@@ -5,7 +5,7 @@
 ** Login   <nicolas@epitech.net>
 ** 
 ** Started on  Mon Apr 28 13:51:55 2014 Nicolas Ades
-** Last update Tue Apr 29 09:40:07 2014 Nicolas Ades
+** Last update Tue Apr 29 09:54:40 2014 Nicolas Ades
 */
 
 #include "main.h"
@@ -18,6 +18,6 @@ int	main(int ac, char **av, char **envp)
   (void)av;
   if (check_env(envp, i_env) == -1)
     return (-1);
-  if (prompt == 0)
+  if (prompt(i_env) == 0)
     return (O);
 }
--- src/core/prompt.c
+++ src/core/prompt.c
@@ -5,17 +5,18 @@
 ** Login   <nicolas@epitech.net>
 ** 
 ** Started on  Mon Apr 28 13:56:09 2014 Nicolas Ades
-** Last update Mon Apr 28 15:08:26 2014 Nicolas Ades
+** Last update Tue Apr 29 09:55:40 2014 Nicolas Ades
 */
 
 #include "core.h"
 
-int	prompt()
+int	prompt(t_env *i_env)
 {
   char	cmd;
 
   while (cmd = get_next_line(0))
     {
+      my_putstr("42sh--> ");
       if (strcmp(cmd, "exit") == 1)
 	return (O);
       parser(cmd);
