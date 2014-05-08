d67ec1cc2e69926b08c4b2eecd302af2622f486c
mediav_j <mediav_j@epitech.eu>
2014-05-08 16:21:51 +0200

    l

---
+++ includes/core.h
@@ -0,0 +1,26 @@
+/*
+** core.h for  in /home/nicolas/Workspace/42sh/includes
+** 
+** Made by Nicolas Ades
+** Login   <nicolas@epitech.net>
+** 
+** Started on  Mon Apr 28 13:51:04 2014 Nicolas Ades
+** Last update Thu May  8 16:21:38 2014 Jeremy Mediavilla
+*/
+
+#ifndef		CORE_H_
+# define	CORE_H_
+
+# include "main.h"
+# include "my_list.h"
+# include "my.h"
+
+/* fonctions dans env.c */
+void		get_all_env(t_list *, char **);
+void		aff_env(t_list *);
+
+/* fonctions dans prompt.c */
+
+int		prompt(t_list *);
+
+#endif /* CORE_H_ */
---
+++ src/core/main.c
@@ -0,0 +1,25 @@
+/*
+** main.c for  in /home/mediav_j/mabm/42sh/src
+** 
+** Made by Jeremy Mediavilla
+** Login   <mediav_j@epitech.net>
+** 
+** Started on  Wed May  7 17:42:26 2014 Jeremy Mediavilla
+** Last update Thu May  8 16:20:40 2014 Jeremy Mediavilla
+*/
+
+#include "core.h"
+
+int		main(int ac, char **av, char **env)
+{
+  t_list	*list;
+
+  (void)ac;
+  (void)av;
+  list = create_list(NULL);
+  get_all_env(list, env);
+  /* aff_env(list); */
+  if (prompt(list) == 0)
+    return (0);
+  return (0);
+}
