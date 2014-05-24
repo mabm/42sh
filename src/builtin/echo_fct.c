9357058d4b5354fbccbaeea8c062c6ab9cff0612
Nicolas ades <nicolas.ades@epitech.eu>
2014-05-24 18:19:49 +0200

    nico

---
+++ src/builtin/echo_fct.c
@@ -0,0 +1,28 @@
+/*
+** echo_fct.c for  in /home/nicolas/Workspace/Unix/42sh/src/builtin
+** 
+** Made by Nicolas Ades
+** Login   <nicolas@epitech.net>
+** 
+** Started on  Sat May 24 17:58:13 2014 Nicolas Ades
+** Last update Sat May 24 18:19:11 2014 Nicolas Ades
+*/
+
+#include "core.h"
+
+void		print_with_opt(int i, char **cmd)
+{
+
+}
+
+int		go_to_arg(char **cmd)
+{
+  int		i;
+
+  while (cmd[i] != NULL)
+    {
+      
+      i++;
+    }
+  return (1);
+}
