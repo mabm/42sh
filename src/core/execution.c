c6111db20edcbe5a83b4b83b49762cfea3e4ab44
Nicolas ades <nicolas.ades@epitech.eu>
2014-05-24 09:29:20 +0200

    nico

--- src/core/execution.c
+++ src/core/execution.c
@@ -5,7 +5,7 @@
 ** Login   <nicolas@epitech.net>
 ** 
 ** Started on  Sat May 24 08:06:43 2014 Nicolas Ades
-** Last update Sat May 24 09:12:32 2014 Nicolas Ades
+** Last update Sat May 24 09:28:17 2014 Nicolas Ades
 */
 
 #include "core.h"
@@ -28,7 +28,7 @@ int		do_fork_bis(t_shell *shell, char **cmd)
   return (0);
 }
 
-int		do_fork(t_shell *shell, char **cmd, char **path)
+int		do_fork(t_shell *shell, char **cmd, char **path, int pos)
 {
   pid_t		ptr;
   
@@ -38,7 +38,7 @@ int		do_fork(t_shell *shell, char **cmd, char **path)
       return (-1);
     }
   if (pid == 0)
-    execve(path, cmd, shell->env);
+    execve(path[pos], cmd, shell->env);
   else
     wait(NULL);
   return (0);
@@ -57,10 +57,18 @@ int		is_cmd_exist(t_shell *shell, char **cmd, char **path)
       memset(tmp, 0, (strlen(path[i]) + 1));
       tmp = strcat(tmp, path[i]);
       tmp = strcart(tmp, cmd[0]);
-      if (access(tmp, X_OK) != -1)
-	return (i);
+      if (access(tmp, F_OK) != -1)
+	{
+	  if (access(tmp, X_OK) == -1)
+	    {
+	      fprintf(stderr, "%s: Permission denied\n", cmd[0]);
+	      return (-1);
+	    }
+	  return (i);
+	}
       i++;
     }
+  fprintf(stderr, "%: Command not found\n", cmd[0]);
   return (-1);
 }
 
@@ -79,7 +87,7 @@ int		my_exec(t_shell *shell, char **cmd)
     return (-1);
   if ((pos = is_cmd_exist(shell, cmd, path)) == -1)
     return (-1);
-  if (do_fork(shell, cmd, path) == -1)
+  if (do_fork(shell, cmd, path, pos) == -1)
     return (-1);
   return (0);
 }
