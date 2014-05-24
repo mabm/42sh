db508ec1441ae9e4b8393ea49c7469881e349241
Nicolas ades <nicolas.ades@epitech.eu>
2014-05-24 16:11:15 +0200

    ajout d'une fonction pour l'exec

---
+++ src/core/exec_fct.c
@@ -0,0 +1,34 @@
+/*
+** exec_fct.c for  in /home/nicolas/Workspace/Unix/42sh/src/core
+** 
+** Made by Nicolas Ades
+** Login   <nicolas@epitech.net>
+** 
+** Started on  Sat May 24 16:09:23 2014 Nicolas Ades
+** Last update Sat May 24 16:10:40 2014 Nicolas Ades
+*/
+
+#include "core.h"
+
+char		**env_in_tab(t_list *list)
+{
+  char		**tab;
+  int		size;
+  int		i;
+  int		len;
+
+  i = 0; 
+  size = list_size(list);
+  tab = xmalloc(size * sizeof(char *));
+  list = list->next;
+  while (list != NULL)
+    {
+      len = my_strlen(list->data) + 1;
+      tab[i] = xmalloc(len * sizeof(char));
+      my_memset(tab[i], '\0', len);
+      tab[i] = list->data;
+      list = list->next;
+      i++;
+    }
+  return (tab);
+}
